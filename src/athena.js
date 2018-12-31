854d78a59a9d6d38ac0f71520fa2fd08097baffc
Gene Ng <fishballium@gmail.com>
2018-12-31 15:13:58 +0800

    Add jsdoc for athena.js

--- src/athena.js
+++ src/athena.js
@@ -1,3 +1,15 @@
+
+/**
+ * Generate Athena query string from the getData() request.
+ *
+ * Rules:
+ * 1. Fields specified in request.fields will be SELECT-ed.
+ * 2. If `dateRangeColumn` is specified, it will be added in the WHERE clause.
+ * 3. If `rowLimit` is specified, it will be added in the LIMIT clause.
+ *
+ * @param {Object} request Data request parameters.
+ * @return {string} The generated query string.
+ */
 function generateAthenaQuery(request) {
   var defaultRowLimit = 1000;
   var params = request.configParams || {};
@@ -21,6 +33,15 @@ function generateAthenaQuery(request) {
   return query;
 }
 
+/**
+ * Submit a query to AWS Athena.
+ *
+ * @param  {string} region Region of the AWS Athena to use.
+ * @param  {string} database Glue datebase to run this query.
+ * @param  {string} query The query string.
+ * @param  {string} outputLocation The S3 path (s3://<bucket>/<path>) to store the query result.
+ * @return {Object} {"QueryExecutionId": "string"}
+ */
 function runAthenaQuery(region, database, query, outputLocation) {
   var payload = {
     'ClientRequestToken': uuidv4(),
@@ -35,6 +56,15 @@ function runAthenaQuery(region, database, query, outputLocation) {
   return AWS.post('athena', region, 'AmazonAthena.StartQueryExecution', payload);
 }
 
+/**
+ * Wait until an Athena query to reach a terminal state.
+ *
+ * This is a blocking function that continuously pull the state of the query.
+ * If the query finished without any errors, the function will return nothing.
+ * Otherwise an exception will be thrown.
+ * @param  {string} region Region of the AWS Athena to use.
+ * @param  {string} queryExecutionId The submitted execution ID.
+ */
 function waitAthenaQuery(region, queryExecutionId) {
   var payload = {
     'QueryExecutionId': queryExecutionId
@@ -57,6 +87,12 @@ function waitAthenaQuery(region, queryExecutionId) {
   }
 }
 
+/**
+ * Fetch all rows from a submitted AWS Athena query.
+ * @param  {string} region Region of the AWS Athena to use.
+ * @param  {string} queryExecutionId The submitted execution ID.
+ * @return {Array} Array of rows, in the form of { col0: val0, col1: val1, ... }
+ */
 function getAthenaQueryResults(region, queryExecutionId) {
   // Get results until all rows are fetched
   var rows = [];
@@ -138,6 +174,12 @@ function queryResultsToRows(schema, queryResults) {
   });
 }
 
+/**
+ * Submit query to AWS Athena and return results as required by Data Studio.
+ *
+ * @param  {Object} request Data request parameters.
+ * @return {Object} Contains the schema and data for the given request.
+ */
 function getDataFromAthena(request) {
   var requestedFieldIds = request.fields.map(function(field) {
     return field.name;
